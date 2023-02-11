ca8e89f8f6f9714f93c80cd45f01a49d6e16d076
yurubahmed <yurubiftin2002@gmail.com>
2023-02-10 22:56:46 -0500

    commit tree 68c715495aa01c8710f8684e66c9d85087755561

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title , :author , :page_count , :genre
+    def initialize (title )
+        @title = title 
 
+    end 
+   def turn_page
+    puts "Flipping the page...wow, you read fast!" 
+   end
+end
 
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "mystery"
+puts "turn the page"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand , :color , :size , :material ,:condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+    
+
+end
+shoe = Shoe.new("Adidas")
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
